52babaf073fae579e83782072f7dd7b17b42732d
victorgabrielms <vgabriel.vg84@gmail.com>
2019-10-26 16:20:19 -0300

    env vars

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,14 +1,24 @@
 import firebase from 'firebase';
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  DATABASE_URL,
+  PROJRCT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+  MEASUREMENT_ID,
+} from 'react-native-dotenv';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAerEDhPUdxVE32h2cuKhkEer_1vdNusjM',
-  authDomain: 'finapp-db-2cea9.firebaseapp.com',
-  databaseURL: 'https://finapp-db-2cea9.firebaseio.com',
-  projectId: 'finapp-db-2cea9',
-  storageBucket: 'finapp-db-2cea9.appspot.com',
-  messagingSenderId: '269650175320',
-  appId: '1:269650175320:web:177a84ded05c40c480b12d',
-  measurementId: 'G-PRQP2TBZNF',
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  databaseURL: DATABASE_URL,
+  projectId: PROJRCT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
 };
 
 // Initialize Firebase
